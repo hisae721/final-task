5e2d71dfa9e84f60bdb275ebaae3562b2411f345
hisae-mishima <721nhnh@gmail.com>
2026-03-20 11:57:46 +0900

    Math.floorと文字列変換追加

--- src/TimeFormatter.ts
+++ src/TimeFormatter.ts
@@ -5,11 +5,36 @@ class TimeFormatter {
     }
 
     formatSeconds(seconds: number): string{
-        const hour = seconds/3600;
+        const hour = Math.floor(seconds/3600);
         const remainder = seconds%3600;
-        const minute = remainder/60;
+        const minute = Math.floor(remainder/60);
         const second = remainder%60;
+
+        let hourString="";
+        if(hour<10){
+            hourString = "0" + hour;
+        }else{
+            hourString=String(hour);
+        }
+
+        let minuteString="";
+        if(minute<10){
+            minuteString = "0" + minute;
+        }else{
+            minuteString=String(minute);
+        }
+
+        let secondString="";
+        if(second<10){
+            secondString = "0" + second;
+        }else{
+            secondString=String(second);
+        }
+
+        return `${hourString}:${minuteString}:${secondString}`;
+        
     }
+
 
     formatHourMinute(hour: number, minute: number): string{
 
