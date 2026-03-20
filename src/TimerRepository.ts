4f502bc17b155234e444620e26fa501449868ca3
hisae-mishima <721nhnh@gmail.com>
2026-03-20 18:36:01 +0900

    TimerRepositoryのremoveメソッドを実装

--- src/TimerRepository.ts
+++ src/TimerRepository.ts
@@ -2,35 +2,43 @@ import { TimerSetting } from "./TimerSetting";
 
 
 export class TimerRepository {
-    private MAX_TIMERS:number;
-    private timers:TimerSetting[];
+    private MAX_TIMERS: number;
+    private timers: TimerSetting[];
 
-    constructor(){
-        this.MAX_TIMERS=5;
-        this.timers=[];
+    constructor() {
+        this.MAX_TIMERS = 5;
+        this.timers = [];
     }
 
-    private add(timer:TimerSetting):void{
-        if(this.timers.length>=this.MAX_TIMERS){
+    private add(timer: TimerSetting): void {
+        if (this.timers.length >= this.MAX_TIMERS) {
             return;
         }
 
         this.timers.push(timer);
     }
 
-    private remove(id:TimerId):void{
+    /**
+     * 指定されたIDのタイマーを一覧から消す
+     * @param id 
+     */
+    private remove(id: TimerId): void {
+        const result = this.timers.filter((timer) => {
+                return id !== timer.id;
+        })
+        this.timers = result;
+    }
+
+    private getById(id: TimerId): TimerSetting {
 
     }
-    private getById(id:TimerId):TimerSetting{
+    private countTimers(): number {
 
     }
-    private countTimers():number{
+    private isFull(): boolean {
 
     }
-    private isFull(): boolean{
-
-    }
-    private getAll():TimerSetting[]{
+    private getAll(): TimerSetting[] {
 
     }
 }
