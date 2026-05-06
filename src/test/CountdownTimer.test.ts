bdea08e382a000994cac0d30b6706845bf0a5459
hisae-mishima <721nhnh@gmail.com>
2026-05-06 22:50:41 +0900

    CountdownTimerのテストコードを追加

--- src/test/CountdownTimer.test.ts
+++ src/test/CountdownTimer.test.ts
@@ -57,6 +57,40 @@ describe("CountdownTimer", () => {
         expect(onFinishedMock).toHaveBeenCalled();
         vi.useRealTimers();
     })
-
+    it("setTime() したら Ready になる", () => {
+        const intervalTicker = new IntervalTicker();
+        const countdownTimer = new CountdownTimer(intervalTicker);
+        countdownTimer.setTime(10);
+        const state = countdownTimer.getState();
+        const duration = countdownTimer.getDuration();
+        expect(state).toBe(TimerState.Ready);
+        expect(duration).toBe(10000);
+    })
+    it("start() したら ticker が動き始める", () => {
+        const intervalTicker = new IntervalTicker();
+        const countdownTimer = new CountdownTimer(intervalTicker);
+        const startSpy = vi.spyOn(intervalTicker, "startRepeatingTick");
+        countdownTimer.setTime(10);
+        countdownTimer.start();
+        expect(startSpy).toHaveBeenCalled();
+    })
+    it("pause() したら ticker が止まる", () => {
+        const intervalTicker = new IntervalTicker();
+        const countdownTimer = new CountdownTimer(intervalTicker);
+        const stopSpy = vi.spyOn(intervalTicker, "stopRepeatingTick");
+        countdownTimer.setTime(10);
+        countdownTimer.start();
+        countdownTimer.pause();
+        expect(stopSpy).toHaveBeenCalled();
+    })
+    it("reset() したら残り時間が元の時間に戻る",()=>{
+        const intervalTicker = new IntervalTicker();
+        const countdownTimer = new CountdownTimer(intervalTicker);
+        countdownTimer.setTime(10);
+        countdownTimer.start();
+        countdownTimer.reset();
+        const duration = countdownTimer.getDuration();
+        expect(duration).toBe(10000);
+    })
 })
 
