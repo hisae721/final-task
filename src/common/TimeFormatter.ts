e5d531e31a0bb4278224ed12c867c37412848446
“daisuuuk” <tabi.nami1397@gmail.com>
2026-05-31 16:31:41 +0900

    アラーム機能の不xーマット形式実装追加

--- src/common/TimeFormatter.ts
+++ src/common/TimeFormatter.ts
@@ -1,3 +1,5 @@
+import { AlarmTime } from "../alarm/domain/alarm/AlarmTime";
+
 /**
  * 時間の値を表示用の文字列に変換するクラス
  * 
@@ -41,5 +43,18 @@ export class TimeFormatter {
         return `${hourString}:${minuteString}:${secondString}`;
         
     }
+
+    // 秒を "MM:SS" 形式にフォーマットする。タイマー向け
+    // formatSeconds(seconds: number): string {}
+
+    // 時・分を "HH:MM" 形式にフォーマットする。アラーム向け
+    formatHourMinute(time: AlarmTime): string {
+        // 「padStart(2, "0")」 = 元の文字列が2桁に満たない場合、先頭に「0」を自動的に補完して合計2桁にする
+        const hourDisplay = time.hour.toString();
+        const minuteDisplay = time.minute.toString().padStart(2, "0");
+        return `${hourDisplay}:${minuteDisplay}`;
+    }
+    // { 例: (9, 5) → "9:05" }
+    // format(time: AlarmTime): string
 
 }
